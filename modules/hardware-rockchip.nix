6743ba010b2bd2a2be6734b2c7c10cc82f83b260
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-12-14 19:37:06 -0500

    hardware-rockchip: Add rockchip-rk3399s

--- modules/hardware-rockchip.nix
+++ modules/hardware-rockchip.nix
@@ -21,6 +21,11 @@ in
       default = false;
       description = "enable when SOC is RK3399S";
     };
+    hardware.socs.rockchip-rk3399s.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "enable when SOC is RK3399S";
+    };
   };
 
   config = mkMerge [
